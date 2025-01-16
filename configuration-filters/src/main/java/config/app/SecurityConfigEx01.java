281f2bfbd6885d9c5656dbf609c7ae6f44f11eba
g2ro <kilho603@gmail.com>
2025-01-16 14:46:25 +0900

    Spring Security #5

--- configuration-filters/src/main/java/config/app/SecurityConfigEx01.java
+++ configuration-filters/src/main/java/config/app/SecurityConfigEx01.java
@@ -1,4 +1,4 @@
-package config.web;
+package config.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +26,29 @@ public class SecurityConfigEx01 {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
+//      http.formLogin().loginPage(null).. // 옛날방식
+    	
+/*    	
+    	http
+        .formLogin(new Customizer<FormLoginConfigurer<HttpSecurity>>() {
+			
+			@Override
+			public void customize(FormLoginConfigurer<HttpSecurity> t) {
+			}
+		}); // UsernamePasswordAuthenticationFilter => loginInterceptor와 유사한 방식, .formLogin(new configure)
+*/
+    	// UsernamePasswordAuthenticationFilter는 로그인만 하는 필터 어디에서 로그인해야하는지 정하는 필터가 아님.
+    	// AuthorizationFilter에 의해서 로그인을 해야하는 위치를 정하는 필터
+    	http
+    		.formLogin((formLogin) -> { // 자바는 함수를 파라미터로 전달할 수 있는 1급 시민이 아님.
+    			/*
+    			formLogin
+    				.loginPage("/user/login")
+    				.usernameParameter("email")
+    				.loginProcessingUrl("/");
+    			// 감시해야할 url과 로그인 페이지 userName등을 설정 => 인터셉터와 유사한 활동을 함, 라이브러리이기 때문에 함수를 이용해서 등록됨.
+    			*/
+    		});
+    	return http.build();
     }
 }
--- configuration-filters/src/main/java/config/app/SecurityConfigEx03.java
+++ configuration-filters/src/main/java/config/app/SecurityConfigEx03.java
@@ -1,4 +1,4 @@
-package config.web;
+package config.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,21 +11,29 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigEx02 {
+public class SecurityConfigEx03 {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return new WebSecurityCustomizer() {
             @Override
             public void customize(WebSecurity web) {
                 web
-                        .ignoring()
-                        .requestMatchers(new AntPathRequestMatcher("/assets/**"));
+                    .ignoring()
+                    .requestMatchers(new AntPathRequestMatcher("/assets/**"));
             }
         };
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        http
+        	.formLogin((formLogin) -> {})
+        	.httpBasic((httpBasic) -> {})
+        	.authorizeHttpRequests((authorizeRequests) -> {
+        		/* Access Control List(ACL) controller에서 어노테이션을 달던 행위를 대체해줄 수 있는 기술 */
+        		authorizeRequests
+        			.anyRequest().permitAll(); // 모두 통과 시키는 행위 -> 상단에서 조건들을 부여하여 해당 조건들이 모두 통과 될때 현재 코드 실행 되도록 작성  // ACL 마지막 코드에 항상 존재해야한다.
+        	});
+    	return http.build();
     }
 }
